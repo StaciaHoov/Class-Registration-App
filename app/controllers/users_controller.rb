be714c0355317df7c80d59f7e2820827256a188e
staciahoov <staciaray@roadrunner.com>
2016-02-24 20:57:14 +0000

    edit User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @students = Student.where(user_id: @user.id)
-   
   end
-
   
   def edit
     @user = current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update, :show]
+  
   root to: 'welcome#index'
   get 'welcome/about'
   get 'courses/planning'
