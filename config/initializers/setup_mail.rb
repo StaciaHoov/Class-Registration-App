4b9a792838d196e053cec3673e8db6d8f9be764d
staciahoov <staciaray@roadrunner.com>
2016-03-08 21:44:22 +0000

    changed smtp

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
-if Rails.env.development? || Rails.env.production?
+if Rails.env.production?
     ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
         address: 'smtp.sendgrid.net',
-        port: '465',
+        port: '587',
         authentication: :plain,
         user_name: ENV['SENDGRID_USERNAME'],
         password: ENV['SENDGRID_PASSWORD'],
