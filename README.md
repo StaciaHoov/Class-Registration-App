60b30bafea710c24cd9fb8fd416e453c624f92bf
staciahoov <staciaray@roadrunner.com>
2016-07-20 23:58:48 +0000

    edits per Jose

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ themselves or their children up to take classes.
     * view and edit their teaching schedule 
 * Members can sign up to teach courses, and designate teaching preferences such as room, time, maximum number of students, fee, age-group, and co-teacher.
 * On the Class Planning page, in-place editing is possible to change course time.
-* Courses can be viewed from the Current Class Offerings table, which shows number of openings per course available. As students register a course,
+* Courses can be viewed from the Current Class Offerings table, which shows number of openings per course. As students register a course,
  the Openings field increments down until all seats are taken. Table then displays 'Course Full'.
 * Class Offerings are tabulated by age-group. Jumping to different tables is possible via anchor links throughout the page.
 * Each table has sortable columns.
--- app/models/course.rb
+++ app/models/course.rb
@@ -11,7 +11,23 @@ class Course < ActiveRecord::Base
     validates :time_block, presence: true
     
     before_destroy :delete_schedules
-
+   
+    def upcount_seats
+        self.increment!(:seats_taken)
+        check_full
+    end
+    
+    def downcount_seats
+        self.decrement!(:seats_taken)
+        check_full
+    end
+      
+      
+    def students_waiting
+        list = self.waitlist
+        WaitlistStudent.where(waitlist_id: list.id)
+    end
+    
     def delete_schedules
         schedule_first_course = Schedule.where(first_course_id: self.id) 
         schedule_second_course = Schedule.where(second_course_id: self.id) 
@@ -28,26 +44,7 @@ class Course < ActiveRecord::Base
         if schedule_third_course.any?
             schedule_third_course.delete_all
         end
-    end
-    
-    
-    def upcount_seats
-        self.increment!(:seats_taken)
-        check_full
-    end
-    
-    
-    def downcount_seats
-        self.decrement!(:seats_taken)
-        check_full
-    end
-      
-      
-    def students_waiting
-        list = self.waitlist
-        WaitlistStudent.where(waitlist_id: list.id)
-    end
-    
+    end    
     
     private
     
